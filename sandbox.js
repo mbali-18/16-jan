c0d6b9b527f92766f923d75bb47727f846bd01b3
mbali-18 <faithfulservent18@gmail.com>
2025-01-17 14:16:12 +0200

    first commit

---
+++ sandbox.js
@@ -0,0 +1,77 @@
+// // functions
+// function name(){
+//     console.log("Mbali");
+// }
+// name("Mbali")
+
+
+// //objects
+// let sandwich = {
+//     bread: "brown",
+//     slices:"two",
+// }
+// console.log(sandwich.bread)
+
+
+// //strings
+// let greeting = "Hi there!";
+// console.log(greeting);
+
+
+// // Numbers
+// let age = 10;
+// console.log(age); 
+
+
+// // Booleans
+// let likesCoding = true;
+// console.log(likesCoding); 
+
+
+// // Arrays
+// let colors = ["red", "blue", "green"];
+// console.log(colors[0]); 
+
+
+// function name(){
+//     console.log("Mbuso");
+// }
+// name();
+
+// let toybox = {
+//     colour:"red",
+//     shape:"cirle",
+// }
+// console.log(toybox.colour);
+
+// // let greeting = "Heyya";
+// // console.log (greeting)
+
+// let age = 21
+// console.log(age)
+
+// let = ilovecode = true
+// console.log(ilovecode);
+
+// let sibiling = [`mbali`,`nkuli`,`lwazi`,`xolani`];
+// console.log(sibiling[0]);
+
+// function wrap(){
+//     console.log("chicken")
+// }
+// wrap();
+
+// let wrap ={
+//     chicken:"spicy",
+//     sauce:"bbq",
+// }
+// console.log(wrap.chicken)
+
+let greet = "heyy";
+console.log (greet)
+
+
+let date = 21;
+console.log(date)
+
+let wrap = []
